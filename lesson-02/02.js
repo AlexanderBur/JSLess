97911ced1e0952659cb1565d6a514f2dcaed5626
AleksanderBu <moroz_bu@proton.me>
2026-03-17 14:35:01 +0500

    les2 fix

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() *6)
-let dice2 = Math.floor(Math.random() *6)
+let dice1 = Math.floor(Math.random() *6) +1
+let dice2 = Math.floor(Math.random() *6) +1пше фвв ю
 let isWinningDouble
 
 if ((dice1 > 3 & dice2 > 3) & (dice1 === dice2)) {
